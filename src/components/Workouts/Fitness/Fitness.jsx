2df957b3d0a329181997cd1e5b2a4314448af702
ZeinabNabil <zeinabnabil3110@gmail.com>
2023-06-22 18:31:52 +0200

    adding videos in workouts and edit meditation details

--- src/components/Workouts/Fitness/Fitness.jsx
+++ src/components/Workouts/Fitness/Fitness.jsx
@@ -12,95 +12,132 @@ import { useEffect } from "react";
 import { getAllWorkouts } from "../../../redux/workoutsSlice";
 import routes from "../../../common/routes";
 import { useNavigation } from "@react-navigation/native";
+import { ResizeMode, Video } from "expo-av";
 
 const Fitness = () => {
-    const { fitnesses, isLoading, error } = useSelector(
-        (state) => state.workouts
-      );
-      const dispatch = useDispatch();
-      useEffect(() => {
-        dispatch(getAllWorkouts());
-      }, []);
-      const { navigate } = useNavigation();
+  const { fitnesses, isLoading, error } = useSelector(
+    (state) => state.workouts
+  );
+  const dispatch = useDispatch();
+  // useEffect(() => {
+  //   dispatch(getAllWorkouts());
+  // }, []);
+  const { navigate } = useNavigation();
   const h = Dimensions.get("window").height;
   const w = Dimensions.get("window").width;
   return (
     <>
-      {isLoading?<View><Text>Loading...</Text></View>:error?<View><Text>{error}</Text></View>:fitnesses?<><FlatList
-        ListHeaderComponent={() => (
-          <View>
-            <ImageBackground
-              style={{ height: h - parseInt("123px") }}
-              source={require("../../../../assets/Images/fitnessGym1.webp")}
-              resizeMode="cover"
-            >
-              <View
-                style={{
-                  backgroundColor: "#00000087",
-                  height: "100%",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <Text
+      {isLoading ? (
+        <View>
+          <Text>Loading...</Text>
+        </View>
+      ) : error ? (
+        <View>
+          <Text>{error}</Text>
+        </View>
+      ) : fitnesses ? (
+        <>
+          <FlatList
+            ListHeaderComponent={() => (
+              <View>
+                <View
                   style={{
-                    color: "white",
-                    fontSize: 25,
-                    fontFamily: styles.fontFamilyBold,
+                    position: "relative",
+                    zIndex: 1,
+                    backgroundColor: "black",
                   }}
                 >
-                  Fitness Workout
+                  <Video
+                    source={require("../../../../assets/videos/gym.mp4")}
+                    style={{ height: 300, position: "absollute", zIndex: 5 }}
+                    // useNativeControls
+                    resizeMode={ResizeMode.COVER}
+                    shouldPlay
+                    isMuted
+                    isLooping
+                    // onPlaybackStatusUpdate={status => setStatus(() => status)}
+                  ></Video>
+                  <View
+                    style={{
+                      position: "absolute",
+                      width: "100%",
+                      height: "100%",
+                      top: 0,
+                      left: 0,
+                      backgroundColor: "#00000087",
+                      zIndex: 10,
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: "white",
+                        fontSize: 25,
+                        fontFamily: styles.fontFamilyBold,
+                      }}
+                    >
+                      Fitness Workout
+                    </Text>
+                  </View>
+                </View>
+                <Text
+                  style={{
+                    ...styles.viewStyle,
+                    textAlign: "center",
+                    fontFamily: styles.fontFamilyBold,
+                    fontSize: styles.header1Size,
+                  }}
+                >
+                  Fitness of the day
                 </Text>
               </View>
-            </ImageBackground>
-            <Text
-              style={{
-                ...styles.viewStyle,
-                textAlign: "center",
-                fontFamily: styles.fontFamilyBold,
-                fontSize: styles.header1Size,
-              }}
-            >
-              Fitness of the day
-            </Text>
-          </View>
-        )}
-        showsVerticalScrollIndicator={false}
-        data={fitnesses}
-        renderItem={({ item }) => (
-          <View style={{ paddingHorizontal: 15 }}>
-            <Pressable
-              onPress={() => {
-                navigate(routes.workoutsDetails,{id:item.id})
-              }}
-              style={{ marginBottom: 15, borderWidth: 1, borderRadius: 10, backgroundColor:"white" }}
-            >
-              <Image
-                style={{
-                  width: w - 32,
-                  height: 320,
-                  borderTopRightRadius: 10,
-                  borderTopLeftRadius: 10,
-                }}
-                source={{uri: item.gifUrl}}
-              ></Image>
-              <Text
-                style={{
-                  color: styles.mainColor,
-                  fontSize: 18,
-                  fontFamily: styles.fontFamilySemiBold,
-                  borderTopWidth: 1,
-                  padding: 10,
-                }}
-              >
-                {item.name}
-              </Text>
-            </Pressable>
-          </View>
-        )}
-        keyExtractor={(fitness)=>fitness.id}
-      ></FlatList></>:<View></View>}
+            )}
+            showsVerticalScrollIndicator={false}
+            data={fitnesses}
+            renderItem={({ item }) => (
+              <View style={{ paddingHorizontal: 15 }}>
+                <Pressable
+                  onPress={() => {
+                    navigate(routes.workoutsDetails, { id: item.id });
+                  }}
+                  style={{
+                    marginBottom: 15,
+                    borderWidth: 1,
+                    borderRadius: 10,
+                    backgroundColor: "white",
+                  }}
+                >
+                  <Image
+                    style={{
+                      width: w - 32,
+                      height: 320,
+                      borderTopRightRadius: 10,
+                      borderTopLeftRadius: 10,
+                    }}
+                    source={{ uri: item.gifUrl }}
+                  ></Image>
+                  <Text
+                    style={{
+                      color: styles.mainColor,
+                      fontSize: 18,
+                      fontFamily: styles.fontFamilySemiBold,
+                      borderTopWidth: 1,
+                      padding: 10,
+                    }}
+                  >
+                    {item.name}
+                  </Text>
+                </Pressable>
+              </View>
+            )}
+            keyExtractor={(fitness) => fitness.id}
+          ></FlatList>
+        </>
+      ) : (
+        <View></View>
+      )}
     </>
   );
 };
